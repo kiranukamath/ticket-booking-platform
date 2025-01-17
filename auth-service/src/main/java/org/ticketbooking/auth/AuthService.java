a4bb3447cda2a2b5847db26f886a75e631e7e3b2
Kiran Kamath <kirankamath@Kirans-MacBook-Air.local>
2025-01-17 16:56:25 +0530

    init

---
+++ auth-service/src/main/java/org/ticketbooking/auth/AuthService.java
@@ -0,0 +1,5 @@
+package org.ticketbooking.auth;
+
+public class AuthService {
+    
+}
---
+++ auth-service/src/test/java/org/ticketbooking/auth/AuthServiceTest.java
@@ -0,0 +1,9 @@
+package org.ticketbooking.auth;
+
+
+class AuthServiceTest {
+    // @Test void appHasAGreeting() {
+    //     App classUnderTest = new App();
+    //     assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    // }
+}
