8a9a3dad78dfc88f21ba2442ec75cfe288aa99c2
Kiran U Kamath <kirankamat.mgm@gmail.com>
2025-01-19 21:37:01 +0530

    auth service changes

--- auth-service/src/main/java/org/ticketbooking/auth/AuthController.java
+++ auth-service/src/main/java/org/ticketbooking/auth/AuthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.ticketbooking.common.model.CommonException;
+import org.ticketbooking.common.exception.CommonException;
 import org.ticketbooking.common.model.User;
 
 @RestController
--- auth-service/src/main/java/org/ticketbooking/auth/UserService.java
+++ auth-service/src/main/java/org/ticketbooking/auth/UserService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.ticketbooking.common.model.CommonException;
+import org.ticketbooking.common.exception.CommonException;
 import org.ticketbooking.common.model.Role;
 import org.ticketbooking.common.model.User;
 import org.ticketbooking.common.repository.RoleRepository;
