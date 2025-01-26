8bd130a73fabd375d837c2f56f60a7722456b101
Kiran U Kamath <kirankamat.mgm@gmail.com>
2025-01-26 21:19:14 +0530

    enable retry

--- src/main/java/org/ticketbooking/TicketBookingPlatformApplication.java
+++ src/main/java/org/ticketbooking/TicketBookingPlatformApplication.java
@@ -2,8 +2,10 @@ package org.ticketbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication(scanBasePackages = "org.ticketbooking")
+@EnableRetry
 public class TicketBookingPlatformApplication {
     public static void main(String[] args) {
         SpringApplication.run(TicketBookingPlatformApplication.class, args);
