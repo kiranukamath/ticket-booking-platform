cda80fd19a98a6d4bdbe73d1cc8daabedcdf1a23
Kiran U Kamath <32927230+kiranukamath@users.noreply.github.com>
2025-03-08 15:41:15 +0530

    kafka consumer with parallel execution (#6)
    kafka consumer with parallel execution using Threadpooltaskexecutor and semaphore

--- booking-service/src/main/java/org/ticketbooking/event/BookingConsumer.java
+++ booking-service/src/main/java/org/ticketbooking/event/BookingConsumer.java
@@ -1,7 +1,9 @@
 package org.ticketbooking.event;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.concurrent.Semaphore;
+
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.ticketbooking.common.model.BookingRequest;
 
@@ -14,11 +16,30 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class BookingConsumer {
 
-    @Autowired
+    private final ThreadPoolTaskExecutor executorService;
+    private final Semaphore semaphore;
     private BookingService bookingService;
 
-    @KafkaListener(topics = "booking-requests", groupId = "booking-group")
+    public BookingConsumer(ThreadPoolTaskExecutor kafkaExecutor, BookingService bookingService){
+        this.executorService = kafkaExecutor;
+        this.semaphore = new Semaphore(executorService.getMaxPoolSize());
+        this.bookingService = bookingService;
+    }
+
+    @KafkaListener(topics = "booking-requests", groupId = "booking-group", concurrency = "1")
     public void consumeBookingRequest(String message) {
+        try {
+            semaphore.acquire();
+            executorService.submit(() -> {
+                processMessage(message);
+                semaphore.release();
+            });
+        } catch (Exception e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void processMessage(String message) {
         BookingRequest request = null;
         try {
             request = new ObjectMapper().readValue(message, BookingRequest.class);
@@ -26,7 +47,7 @@ public class BookingConsumer {
             // throw new CommonException("Booking Request not parsable");
             log.error("ERROR" + message, ex);
         }
-        
+
         if (request != null) {
             try {
                 bookingService.bookTicket(request);
